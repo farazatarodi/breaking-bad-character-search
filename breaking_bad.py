ee308201447d16394a761de4519552da86189dad
farazatarodi <farazatarodi@gmail.com>
2021-04-30 03:01:07 +0200

    Added description

--- breaking_bad.py
+++ breaking_bad.py
@@ -4,6 +4,19 @@ import requests as req
 """
 some explanation:
 
+First when I saw the description I assumed that the characters have an episode attribute.
+However this was not the case, so I had to check characters in each episode. My intial plan was to send request for each episode,
+but that is not optimal (There is a limit for requests from the API).
+I got the list of all episodes and went through them and checked the characters for a match. Created a dictionary with the number
+of occurance for each episode and used the occurance to compare with the number of the characters that we are looking for.
+This way we are not limited to maximum 2 characters.
+Next I created a list of all common episodes between characters and used those episode IDs for getting the data from the intial request.
+One thing I want to mention is that I assumed that an episode with ID 'i' is the episode with index 'i-1' in thr request dictionary.
+I could code it in a way that it would find the episode ID but it required extra work and extra loops.
+(I just wanted to let you know that I did it on purpose, don't judge me)
+Then I simply formatted a string for the output. Finally I checked the status code in case that we get an error from the server.
+I checked the output with pytest and it was 100%. B)
+That's it!
 """
 
 
